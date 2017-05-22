b1d74aa0e585b6f5b7440284380999d37e19dbe0
marlbones <mdimaanods@gmail.com>
2017-05-22 18:10:32 +1000

    Added getWeather function into App.js. Passed states through as props and rendered Weather component

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,1 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
+
+class Weather extends Component {
+
+    render() {
+
+      return (
+        <div>
+          <p>{this.props.city}</p>
+          <p>{this.props.temp}&#176;C</p>
+          <p>{this.props.sky}</p>
+          <p>{this.props.weather}</p>
+        </div>
+
+      )
+    }
+}
+
+export default Weather;
